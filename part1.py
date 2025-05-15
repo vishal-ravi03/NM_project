e3bc6911513d973b47c83441ee07a03569d54a6c
vishal-ravi03 <vishalravichandran2785@gmail.com>
2025-05-15 08:02:40 +0530

    Update part1.py

--- part1.py
+++ part1.py
@@ -4,7 +4,7 @@ from langchain_groq import ChatGroq
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-# from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
